093146d65d06c42674598b553acc51131ae8eae6
Leon <siopao.carleo1114@gmail.com>
2025-07-29 10:23:46 +0800

    modified:   SQLScripts/nextgen.sql

--- SQLScripts/nextgen.sql
+++ SQLScripts/nextgen.sql
@@ -17,3 +17,27 @@
 		   group by c.cid,a.acc, a.principal , a.prin ,a.interest , a.intr 
 		   ) x 
 		   where loanbal > 0 
+
+
+
+-- get CBU Balance As of
+with TrnBal as (
+select acc , sum(balance ) balance
+	from  (
+	SELECT a.acc,
+           SUM(CASE WHEN trntype % 2 = 1 THEN trnamt ELSE -trnamt END) AS balance
+    FROM trandetailshistory t
+    inner join accounts a on a.acc = t.acc and a.accttype = 60
+    GROUP BY a.acc
+    union
+    SELECT a.acc,
+           SUM(CASE WHEN trntype % 2 = 1 THEN trnamt ELSE -trnamt END) AS balance
+    FROM transactiondetails t
+    inner join accounts a on a.acc = t.acc and a.accttype = 60
+    GROUP BY a.acc
+    )trn
+    group by acc
+)
+select a.acc,a.balance,t.balance from accounts a 
+inner join trnbal t on t.acc = a.acc
+where a.balance <> t.balance
