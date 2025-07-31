32911efac99ef01f240d3d37c44e541b521dcd77
Leon <siopao.carleo1114@gmail.com>
2025-07-31 08:59:14 +0800

    modified:   WaivedInterest/v2/colsht_v2.sql 	new file:   WaivedInterest/v2/v3/colsht.sql 	new file:   WaivedInterest/v2/v3/colshtSum.sql 	new file:   WaivedInterest/v2/v3/currentloanbal.sql 	new file:   WaivedInterest/v2/v3/usp_updlntran3.sql

---
+++ WaivedInterest/v2/v3/currentloanbal.sql
@@ -0,0 +1,27 @@
+ ALTER FUNCTION CurrentLoanBal            
+     (@Acc as VarChar(22))            
+    RETURNS TABLE            
+    AS            
+            
+    RETURN             
+            
+    SELECT m.Status, m.Acc,            
+           Sum(a.Prin-CASE WHEN InstPD>a.IntR           THEN             
+                                  CASE WHEN InstPD-a.IntR>a.Prin THEN a.Prin ELSE InstPD-a.IntR END              
+                                                                          ELSE 0      END) BalPrin,            
+           Sum(a.IntR-CASE WHEN InstPD>a.IntR           THEN a.IntR             ELSE InstPD  END)  BalInt,            
+           Sum(IsNull(a.Oth,0)-CASE WHEN InstPD>a.IntR+a.Prin THEN InstPD-a.IntR-a.Prin ELSE 0      END)  BalOth,            
+           Sum( CASE            
+                          when m.DOMATURITY >= ebsysdate and  amortCnt  =1 and m.accttype not in (420,461,475,323,321,483)       
+                          then m.INTEREST - CEILING((m.interest  / 7 ) * CEILING(DATEDIFF(DAY, m.disbdate, ebsysdate) / 7.0))     
+                          WHEN DueDate-DatePart(dw,DueDate)+DatePart(dw,dbo.RefDueDate(m.Frequency,ebsysDate,0)) >             
+                          dbo.RefDueDate(m.Frequency,ebSysDate,0) --AND IsNull(WaivableInt,1) = 1   
+                          and duedate <= DOMATURITY + 6 - Datepart(dw,domaturity)    
+                          and m.accttype not in (420,461,475,323,321,483)       
+                          THEN a.IntR      
+                          ELSE 0 END) WaivedInt            
+      FROM lnMaster m       
+      INNER JOIN LoanInst a  ON m.Acc = a.Acc      
+      inner join (select acc, count(*) amortCnt from loaninst  group by acc)a2 on a2.acc = a.acc, OrgParms            
+      WHERE m.Acc = @Acc            
+      Group by m.Acc,m.Status
